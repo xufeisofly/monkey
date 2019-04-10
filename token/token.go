f7fe22e41b630a5b14ec50efd5b015cb200bd091
Norris <feixu@tju.edu.cn>
2019-04-10 21:49:32 +0800

    first commit

---
+++ token/token.go
@@ -0,0 +1,32 @@
+package token
+
+type TokenType string
+
+// Token struct
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// token constants
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
